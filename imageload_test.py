c2f89fea2bd704e16b19acc2f26a4721d671a6c7
Reed Martz <rmartz@sevone.com>
2014-01-12 17:37:21 -0500

    Adds tests for getting the odds for a specific image.

--- imageload_test.py
+++ imageload_test.py
@@ -1,6 +1,7 @@
 #!/opt/local/bin/python
 from imageload import ImageLibrary, GetImageList
 from pprint import pprint
+from random import choice
 
 try:
 	f = open('./testing_dir.path')
@@ -9,9 +10,18 @@ except IOError:
 	dir = './img/'
 print dir
 
+print "Testing GetImageList"
 pprint(GetImageList(dir))
 
 library = ImageLibrary()
 library.load(dir)
 
+print "Testing library.load"
 pprint(library.tagset.flatten())
+
+print "Testing library.GetOdds"
+image = choice(library.images)
+pprint(image.tags)
+pprint(image.GetOdds())
+
+print "Done"	
