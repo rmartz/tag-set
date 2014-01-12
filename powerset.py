296bf640463f3faed4a9f966b63a875917d81713
Reed Martz <rmartz@sevone.com>
2014-01-12 10:06:22 -0500

    Adds a lander function that copies the list to prevent destructive behavior.

--- powerset.py
+++ powerset.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 
 def PowerSet(start):
+	# Copy the list before we modify it
+	arg = list(start)
+	return __PowerSet(arg)
+
+def __PowerSet(start):
 	if not start:
 		return [ [] ]
-	
+
 	first = start.pop(0)
-	subsets = PowerSet(start)
+
+	subsets = __PowerSet(start)
 
 	result = []
 	for set in subsets:
