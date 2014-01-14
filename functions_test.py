edd37093a92b85291d5e214026a2976a11b64405
Reed Martz <rmartz@sevone.com>
2014-01-13 21:02:56 -0500

    Cleans up the functions test code, and makes it work with the new generator functions.

--- functions_test.py
+++ functions_test.py
@@ -3,17 +3,24 @@
 from functions import PowerSet, OddManOut,MergeDictionary
 from pprint import pprint
 
-pprint(PowerSet([ ]))
-pprint(PowerSet([ 'a' ]))
-pprint(PowerSet([ 'a', 'b' ]))
-pprint(PowerSet([ 'a', 'b', 'c' ]))
-pprint(PowerSet([ 'a', 'b', 'c', 'd' ]))
+def test(function, arg):
+	result = []
+	for value in function(arg):
+		result.append(value)
+	return result
+def testMany(function, args):
+	print "Testing {}:".format(function.__name__)
+	for arg in args:
+		pprint(test(function, arg))
 
-pprint(OddManOut(['a']))
-pprint(OddManOut(['a', 'b']))
-pprint(OddManOut(['a', 'b', 'c']))
-pprint(OddManOut(['a', 'b', 'c', 'd']))
+# Generate a sequence of letters - [[a], [a,b], ...]
+alphabet = [map(chr, range(97, 97+n)) for n in range(1,5)]
 
+testMany(PowerSet, alphabet)
+
+testMany(OddManOut, alphabet)
+
+print "Testing MergeDictionary:"
 pprint(MergeDictionary({'a': 'good'}, {}))
 pprint(MergeDictionary({'a': 'good'}, {'b': 'good'}))
 pprint(MergeDictionary({'a': 'bad', 'b': 'good'}, {'a': 'good'}))
